f1ec2e83c85c9951485294400213d7ae384c55bc
sajeeb-code <mdsajeebprodhan675@gmail.com>
2020-11-22 12:08:53 +0600

    db

--- database/migrations/2020_11_17_171832_create_orders_table.php
+++ database/migrations/2020_11_17_171832_create_orders_table.php
@@ -14,7 +14,7 @@ class CreateOrdersTable extends Migration
     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('o_id');
             $table->integer('customer_id');
             $table->string('order_date');
             $table->string('order_status');
